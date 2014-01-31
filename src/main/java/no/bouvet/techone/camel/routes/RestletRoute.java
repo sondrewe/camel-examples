617ed2de9d8ae61bd290e085fb9e6cc87a27e00d
Sondre Wabakken Engell <sondrewe@gmail.com>
2014-01-31 11:27:44 +0100

    Lagt til lesing fra mongoDB

--- src/main/java/no/bouvet/techone/camel/routes/RestletRoute.java
+++ src/main/java/no/bouvet/techone/camel/routes/RestletRoute.java
@@ -2,6 +2,7 @@ package no.bouvet.techone.camel.routes;
 
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mongodb.MongoDbConstants;
 
 /**
  * Created by felles on 30.01.14.
@@ -10,10 +11,19 @@ public class RestletRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("restlet:http://localhost:8181/myWebservice/{id}")
-                .log(LoggingLevel.INFO, "Mottok melding med id ${header.id}")
-                .setBody().simple("${header.id} er mottatt!")
+        from("restlet:http://localhost:8181/weatherService/windDirection/{direction}")
+                .log(LoggingLevel.INFO, "Mottok forespørsel om vindretning ${header.direction}")
+                .setBody().simple("{\"location.windDirection.@name\" : \"${header.direction}\"}")
+                .setHeader(MongoDbConstants.FIELDS_FILTER, constant("{\"@from\":1, \"@to\":1, \"location.windSpeed\":1}"))
+                .to("mongodb:dbConnection?database=weather&collection=hourly&operation=findAll")
                 ;
+
+        from("restlet:http://localhost:8181/weatherService/tempBelow/{temp}")
+                .log(LoggingLevel.INFO, "Mottok forespørsel om temperatur under ${header.temp}")
+                .setBody().simple("{\"location.temperature.@value\" : {$lt : \"${header.temp}\"}}")
+                .setHeader(MongoDbConstants.FIELDS_FILTER, constant("{\"@from\":1, \"@to\":1, \"location.temperature\":1}"))
+                .to("mongodb:dbConnection?database=weather&collection=hourly&operation=findAll")
+        ;
 
     }
 }
